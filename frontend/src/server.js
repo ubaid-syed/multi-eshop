4cfab8d7fb7d9515d07cc06aaba009b075cfd923
ubaid-syed <arooj.ubaid777@gmail.com>
2026-05-07 19:43:08 +0500

    changes

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,1 @@
-export const server = "https://multi-eshop-production.up.railway.app/api/v2";
-// export const server = " http://localhost:8000/api/v2";
+export const server = process.env.REACT_APP_SERVER;
